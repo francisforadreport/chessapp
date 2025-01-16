a7216dac8714448eb675d9345510b92e543c43f4
Sangeeth Francis James <sangeeth@adreport.io>
2025-01-15 23:52:12 -0500

    Update backend to use dynamic PORT for deployment

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,21 @@ const cors = require("cors");
 
 // Middleware to parse JSON
 app.use(cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "http://localhost:3000", // Update to deployed frontend URL after deployment
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
 }));
 app.use(express.json());
+
 // Use the route files
 app.use("/users", userRoutes);
 app.use("/games", gameRoutes);
-
 
 // Default route
 app.get("/", (req, res) => {
     res.send("Chess app backend is running!");
 });
 
-const PORT = 5001;
+// Use process.env.PORT for deployment and fallback to 5001 locally
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
